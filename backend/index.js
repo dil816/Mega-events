d25ff227780a84cf06f8e08501d69d41d63082cd
Deshan Kodithuwakku <62970260+Deshankodithuwakku@users.noreply.github.com>
2024-05-05 22:10:07 +0530

    All Done

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express, { request, response } from "express";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import { Salary } from "./models/salaryModel.js";
+import salaryRoute from './routes/salaryRoute.js'
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+
+app.get("/", (request, response) => {
+    console.log(request);
+    return response.status(234).send(`Welcome to mern stack tutorial`);
+});
+
+app.use('/salary',salaryRoute);
+
+
+       
+mongoose.
+connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to database:', error);
+    });
