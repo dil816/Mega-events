b5d7b780bd04b92ffb13fa2851d78fb2d8169c04
dil816 <Rivindudilochana18329@gmail.com>
2024-05-06 11:23:37 +0530

    image added

--- backend/middleware/requirePhoto.js
+++ backend/middleware/requirePhoto.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     return cb(
       null,
-      "C:/Users/Rivindu/My_Project/my-function/frontend/public/images"
+      "C:/Users/Rivindu/Desktop/mega/Mega-events/frontend/public/images"
     );
   },
   filename: function (req, file, cb) {
