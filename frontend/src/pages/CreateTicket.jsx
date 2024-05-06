ca41d90815bc68203b0fdd7ae46201669042f495
Osu2002 <it22257468@my.sliit.lk>
2024-05-07 01:58:50 +0530

    format

--- frontend/src/pages/CreateTicket.jsx
+++ frontend/src/pages/CreateTicket.jsx
@@ -5,8 +5,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
 
-import backgroundImage from '../assets/TicketAssets/back2.jpg'
-
+import backgroundImage from "../assets/TicketAssets/back2.jpg";
 
 const CreateTicket = () => {
   const [name, setName] = useState("");
@@ -24,7 +23,6 @@ const CreateTicket = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
-
     // Name validation
     if (!/^[A-Za-z]+$/.test(name)) {
       enqueueSnackbar("Please enter letters only for Name", {
@@ -98,111 +96,110 @@ const CreateTicket = () => {
   };
 
   return (
-
     <div
-      className='p-4'
+      className="p-4"
       style={{
-        backgroundImage:` url(${backgroundImage})`, // Set background image
-        backgroundSize: 'cover', // Adjust background image size
-        backgroundPosition: 'center', // Adjust background image position
-        minHeight: '100vh', // Ensure the background covers the entire screen
+        backgroundImage: ` url(${backgroundImage})`, // Set background image
+        backgroundSize: "cover", // Adjust background image size
+        backgroundPosition: "center", // Adjust background image position
+        minHeight: "100vh", // Ensure the background covers the entire screen
       }}
     >
-      
-    
-    <div className="p-4">
-      <BackButton />
-      <h1 className="text-3xl text-blue-800 my-4">Create Ticket</h1>
-      {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Name</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
+      <div className="p-4">
+        <BackButton />
+        <h1 className="text-3xl text-blue-800 my-4">Create Ticket</h1>
+        {loading ? <Spinner /> : ""}
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Email</label>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Reg_No</label>
+            <input
+              type="string"
+              value={registrationNumber}
+              onChange={(e) => setRegistrationNumber(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Event_Type</label>
+            <input
+              type="text"
+              value={eventType}
+              onChange={(e) => setEventType(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Contact_No</label>
+            <input
+              type="string"
+              value={contactNumber}
+              onChange={(e) => setContactNumber(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Req_type</label>
+            <input
+              type="text"
+              value={requestType}
+              onChange={(e) => setRequretype(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Subject</label>
+            <input
+              type="text"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">Message</label>
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMassage(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-blue-800">File</label>
+            <input
+              type="File"
+              onChange={(e) => setFile(e.target.files[0])}
+              className="border-2 border-gray-500 px-4 py-2  w-full "
+            />
+          </div>
+          <button
+            className="p-2 bg-sky-300 text-blue-900 m-8"
+            onClick={handleSaveBook}
+          >
+            Save
+          </button>
         </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Email</label>
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Reg_No</label>
-          <input
-            type="string"
-            value={registrationNumber}
-            onChange={(e) => setRegistrationNumber(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Event_Type</label>
-          <input
-            type="text"
-            value={eventType}
-            onChange={(e) => setEventType(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Contact_No</label>
-          <input
-            type="string"
-            value={contactNumber}
-            onChange={(e) => setContactNumber(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Req_type</label>
-          <input
-            type="text"
-            value={requestType}
-            onChange={(e) => setRequretype(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Subject</label>
-          <input
-            type="text"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">Message</label>
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMassage(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-blue-800">File</label>
-          <input
-            type="File"
-            onChange={(e) => setFile(e.target.files[0])}
-            className="border-2 border-gray-500 px-4 py-2  w-full "
-          />
-        </div>
-        <button className="p-2 bg-sky-300 text-blue-900 m-8" onClick={handleSaveBook}>
-          Save
-        </button>
       </div>
-    </div>
 
-
-    <footer className="footer p-10 bg-base-200 text-base-content">
+      <footer className="footer p-10 bg-base-200 text-base-content">
         <aside>
           <svg
             width="50"
@@ -243,7 +240,6 @@ const CreateTicket = () => {
         </nav>
       </footer>
     </div>
-    
   );
 };
 
