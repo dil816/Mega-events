8732fd7b8bb609c69c3cd7e7710204844e57c4d0
Kavindu49 <kavindupinsara2003@gmail.com>
2024-05-07 00:33:30 +0530

    format code

--- frontend/src/pages/Bookseat.jsx
+++ frontend/src/pages/Bookseat.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import BackButton from "../components/transport/BackButton";
 
-import backgroundImage from '../assets/TransportAssets/blur3.jpg'
+import backgroundImage from "../assets/TransportAssets/blur3.jpg";
 
 const Cart = () => {
   const { id } = useParams();
@@ -55,91 +55,91 @@ const Cart = () => {
 
   const handleSendReport = () => {
     const phoneNumber = "+94767958695";
-    const message = 'hi';
-    const WhatsAppUrl = 'https://web.whatsapp.com/send?phone=${phoneNumber}&text={encodeURIComponent(message)}';
+    const message = "hi";
+    const WhatsAppUrl =
+      "https://web.whatsapp.com/send?phone=${phoneNumber}&text={encodeURIComponent(message)}";
 
     window.open(whatsappUrl, "_blank");
-  }
+  };
 
   return (
-
     <div
-      className='p-4'
+      className="p-4"
       style={{
         backgroundImage: `url(${backgroundImage})`, // Set background image
-        backgroundSize: 'cover', // Adjust background image size
-        backgroundPosition: 'center', // Adjust background image position
-        minHeight: '100vh', // Ensure the background covers the entire screen
+        backgroundSize: "cover", // Adjust background image size
+        backgroundPosition: "center", // Adjust background image position
+        minHeight: "100vh", // Ensure the background covers the entire screen
       }}
     >
-    
-    <div>
-      
-      <br></br>
-      <br></br>
-      <BackButton />
-      <br></br>
-      <br></br>
+      <div>
+        <br></br>
+        <br></br>
+        <BackButton />
+        <br></br>
+        <br></br>
 
-      
+        <p className="text-green uppercase tracking-wide font-medium text-lg">
+          selected bus
+        </p>
 
-      <p className='text-green uppercase tracking-wide font-medium text-lg'>selected bus</p>
-      
-      <br></br>
+        <br></br>
 
-      <div className="overflow-x-auto">
-        <table className="table table-zebra">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Bus ID</th>
-              <th>Destination</th>
-              <th>Seat Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td> {order._id}</td>
-              <td> {order.destination}</td>
-              <td>{order.price}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Bus ID</th>
+                <th>Destination</th>
+                <th>Seat Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td> {order._id}</td>
+                <td> {order.destination}</td>
+                <td>{order.price}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className="my-4">
+          <label className="my-2 text-gray-500 label">Quantity</label>
+          <input
+            type="number"
+            value={quantity}
+            onChange={handleQuantityChange}
+            className="border-2 border-gray-500 px-4 py-2  w-full"
+            min="1" // Set minimum value to 1
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="my-2 text-gray-500 label">Total Amount</label>
+          <input
+            type="text"
+            value={`${totalAmount}`}
+            readOnly
+            className="border-2 border-gray-500 px-4 py-2  w-full"
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <button onClick={handlePayment} className="btn btn-primary mt-4">
+            Pay Here
+          </button>
+        </div>
       </div>
-
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <div className="my-4">
-        <label className="my-2 text-gray-500 label">Quantity</label>
-        <input
-          type="number"
-          value={quantity}
-          onChange={handleQuantityChange}
-          className="border-2 border-gray-500 px-4 py-2  w-full"
-          min="1" // Set minimum value to 1
-        />
-      </div>
-
-      <div className="my-4">
-        <label className="my-2 text-gray-500 label">Total Amount</label>
-        <input
-          type="text"
-          value={`${totalAmount}`}
-          readOnly
-          className="border-2 border-gray-500 px-4 py-2  w-full"
-        />
-      </div>
-
-      <div className="flex justify-center">
-        <button onClick={handlePayment} className="btn btn-primary mt-4">Pay Here</button>
-      </div>
-    </div>
     </div>
   );
 };
