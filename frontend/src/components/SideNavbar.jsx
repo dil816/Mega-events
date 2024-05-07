4ad3736a96a946f5bc4fdeccd3465e2c61f72cd5
dil816 <Rivindudilochana18329@gmail.com>
2024-05-08 03:18:25 +0530

    bg color removed

--- frontend/src/components/SideNavbar.jsx
+++ frontend/src/components/SideNavbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const SideNavbar = () => {
   return (
     <>
-      <div className="flex-[5%] bg-gray-200 p-[20px]">
+      <div className="flex-[5%]  p-[20px]">
         <div className="grid  [@media_screen_and(max-width:700px)]:flex-col">
           <Link to={"/events"}>
             <a className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white">
